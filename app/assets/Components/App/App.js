d8edbebbf8561e728f796c24cc13ae2d3c08dcfe
John Binning <john.binning17@gmail.com>
2017-05-23 15:18:13 -0600

    add file structure to move app and Map to own components

---
+++ app/assets/Components/App/App.js
@@ -0,0 +1,23 @@
+import React, { Component } from 'react'
+import GMap from '../Map/GMap'
+
+class App extends Component {
+
+  componentDidMount() {
+    // INSERT API CALL TO YOUR INTERNAL API
+  }
+
+  render() {
+    return (
+      <div>
+
+        <h1 className='title-Wander'>Wander<span className='title-List'>List</span></h1>
+        <GMap mapElement={ <div className='mapelement' style={{ height: "300px"}}/> }
+              containerElement={ <div className='containerElement' style={{ height: "300px"}}/> }/>
+        <div>after the map</div>
+      </div>
+    )
+  }
+}
+
+export default App
--- app/index.js
+++ app/index.js
@@ -1,25 +1,27 @@
 import React, { Component } from 'react'
 import { render } from 'react-dom'
 
-import GMap from './assets/Components/GMap'
+import App from './assets/Components/App/App'
 
-class App extends Component {
 
-  componentDidMount() {
-    // INSERT API CALL TO YOUR INTERNAL API
-  }
 
-  render() {
-    return (
-      <div>
-
-        <h1>Hello World</h1>
-        <GMap mapElement={ <div className='mapelement' style={{ height: "300px"}}/> }
-              containerElement={ <div className='containerElement' style={{ height: "300px"}}/> }/>
-        <div>after the map</div>
-      </div>
-    )
-  }
-}
+// class App extends Component {
+//
+//   componentDidMount() {
+//     // INSERT API CALL TO YOUR INTERNAL API
+//   }
+//
+//   render() {
+//     return (
+//       <div>
+//
+//         <h1>Hello World</h1>
+//         <GMap mapElement={ <div className='mapelement' style={{ height: "300px"}}/> }
+//               containerElement={ <div className='containerElement' style={{ height: "300px"}}/> }/>
+//         <div>after the map</div>
+//       </div>
+//     )
+//   }
+// }
 
 render(<App />, document.getElementById('main'))
