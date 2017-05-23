a49a107e13a9b8ef423820cf667b4cd97ef40262
John Binning <john.binning17@gmail.com>
2017-05-23 15:36:39 -0600

    clean up comments and add favicon

--- app/index.js
+++ app/index.js
@@ -3,25 +3,4 @@ import { render } from 'react-dom'
 
 import App from './assets/Components/App/App'
 
-
-
-// class App extends Component {
-//
-//   componentDidMount() {
-//     // INSERT API CALL TO YOUR INTERNAL API
-//   }
-//
-//   render() {
-//     return (
-//       <div>
-//
-//         <h1>Hello World</h1>
-//         <GMap mapElement={ <div className='mapelement' style={{ height: "300px"}}/> }
-//               containerElement={ <div className='containerElement' style={{ height: "300px"}}/> }/>
-//         <div>after the map</div>
-//       </div>
-//     )
-//   }
-// }
-
 render(<App />, document.getElementById('main'))
