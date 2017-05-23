6e5f8e7467b8659ee43f3810174ea95fe1c9a718
John Binning <john.binning17@gmail.com>
2017-05-23 15:29:41 -0600

    delete old comments

--- app/assets/Components/Map/GMap.js
+++ app/assets/Components/Map/GMap.js
@@ -2,11 +2,6 @@ import React from 'react'
 
 import { withGoogleMap, GoogleMap, Marker } from 'react-google-maps'
 
-
-//ref={ props.onMapLoad }
-// { props.markers && renderSingleMarker(props.markers) }
-//{ props.destinations.length && renderAllMarkers(props.destinations) }
-//onClick={(event) => props.addMarker && props.addMarker(event)}>
 const GMap = withGoogleMap(() => {
   console.log(GoogleMap)
   return (
@@ -25,40 +20,3 @@ const GMap = withGoogleMap(() => {
 })
 
 export default GMap
-
-// import React, { Component } from 'react'
-// import { withGoogleMap, GoogleMap, Marker } from "react-google-maps";
-//
-// // Wrap all `react-google-maps` components with `withGoogleMap` HOC
-// // and name it GettingStartedGoogleMap
-// export const GettingStartedGoogleMap = withGoogleMap(props => (
-//   <GoogleMap
-//     ref={props.onMapLoad}
-//     defaultZoom={3}
-//     defaultCenter={{ lat: -25.363882, lng: 131.044922 }}
-//     onClick={props.onMapClick}
-//   >
-//     {props.markers.map((marker, index) => (
-//       <Marker
-//         {...marker}
-//         onRightClick={() => props.onMarkerRightClick(index)}
-//       />
-//     ))}
-//   </GoogleMap>
-// ));
-// // Then, render it:
-// render(
-//   <GettingStartedGoogleMap
-//     containerElement={
-//       <div style={{ height: `100%` }} />
-//     }
-//     mapElement={
-//       <div style={{ height: `100%` }} />
-//     }
-//     onMapLoad={_.noop}
-//     onMapClick={_.noop}
-//     markers={markers}
-//     onMarkerRightClick={_.noop}
-//   />
-//   /
-// );
