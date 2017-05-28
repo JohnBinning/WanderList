d512545899876d6cfd9dbbd85c13dc5908715625
John Binning <john.binning17@gmail.com>
2017-05-28 13:58:41 -0600

    add list helpers

--- app/assets/Components/List/List.js
+++ app/assets/Components/List/List.js
@@ -1,51 +1,12 @@
 import React, { Component } from 'react'
 
-import ListItem from '../ListItem/ListItem'
+import { createListItem } from '../../Helpers/List/ListHelper'
 
 class List extends Component {
 
-  setFilter() {
-    let newArray = this.props.dreams
-    if(this.props.currentFilter === 'showInProgress' ) {
-      const inProg = this.props.dreams.filter( dream => {
-        return dream.completed === false
-      })
-      newArray = inProg
-    }
-
-    if(this.props.currentFilter === 'showCompleted' ) {
-      const showComp = this.props.dreams.filter( dream => {
-        return dream.completed === true
-      })
-      newArray = showComp
-    }
-    return newArray
-  }
-
-  createListItem() {
-    if(this.props.dreams.length){
-      const filtered = this.setFilter()
-
-      return filtered.map( dream => {
-        return (
-            <ListItem
-              coordinates={dream.coordinates}
-              key={dream.id}
-              completeItem={this.props.completeItem}
-              completedStatus={dream.completed}
-              deleteItem={this.props.deleteItem}
-              location={dream.dreamLocation}
-              id={dream.id}
-              body={dream.dreamBody}/>
-        )
-      })
-    }
-    return <div className="no-items">Input a dream to get started!</div>
-  }
-
   render(){
     return(
-      <section className="list-grid">{this.createListItem()}</section>
+      <section className="list-grid">{createListItem(this)}</section>
     )
   }
 }
