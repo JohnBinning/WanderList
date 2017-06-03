bb6091b52189d2816fb3c18bd340b08dbb4abd55
John Binning <john.binning17@gmail.com>
2017-06-03 02:05:13 -0600

    add mapCont test

--- app/assets/Components/Map/GMap.test.js
+++ app/assets/Components/Map/GMap.test.js
@@ -2,12 +2,20 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import fetchMock from 'fetch-mock'
 import { shallow, mount } from 'enzyme'
+import { Marker, HeatmapLayer } from 'react-google-maps'
+
+import GMap from '../Map/GMap'
 
 
 describe('GMap instantiation', () => {
 
-  it('should placeholder', () => {
-
+  it.skip('should placeholder', () => {
+    let mapWidth = "600px"
+    const mapHeight = "600px"
+    const div = document.createElement('div')
+    ReactDOM.render(<GMap mapElement={ <div className='map-element' style={{ height: mapHeight, width: mapWidth}}/> }
+    containerElement={ <div className='container-element' style={{ height: mapHeight, width: mapWidth}}/>}
+                      />, div)
   })
 
 })
--- app/assets/Components/MapContainer/MapContainer.test.js
+++ app/assets/Components/MapContainer/MapContainer.test.js
@@ -2,12 +2,21 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import fetchMock from 'fetch-mock'
 import { shallow, mount } from 'enzyme'
+import { Marker, HeatmapLayer } from 'react-google-maps'
 
+import GMap from '../Map/GMap'
+import MapContainer from './MapContainer'
 
 describe('MapContainer instantiation', () => {
 
-  it('should placeholder', () => {
+  const mockFn = jest.fn()
+
+  it.skip('should render without crashing', () => {
+    const div = document.createElement('div')
+    ReactDOM.render(<MapContainer
+                      />, div)
 
   })
+
 
 })
