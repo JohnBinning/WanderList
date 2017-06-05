6b941a34a04533acf3a35d8783c4a964c7bda3c1
John Binning <john.binning17@gmail.com>
2017-06-05 02:49:59 -0600

    clean up tests

--- app/assets/Components/App/App.test.js
+++ app/assets/Components/App/App.test.js
@@ -50,7 +50,6 @@ describe('App instantiation', () => {
         currentFilter: 'showAll',
         bucketList: [
           {completed: true, coordinates: {lat: 39.7392358, lng: -104.990251}, dreamBody: 'eat', dreamLocation: 'Denver', id: 1496169687545, weatherLocation: {local: "Denver", regional: "CO"}},
-          // {completed: false, coordinates: {lat: 60.124167, lng: 6.74}, dreamBody: 'hike', dreamLocation: 'Trolltunga', id: 1496169692648, weatherLocation: {local: "Trolltunga", regional: "Hordaland"}},
         ]
       }
     }
